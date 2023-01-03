263d12b786c584b2255defc9b8bc17df3f4749cd
CosmicLatte009 <ohhm009@gmail.com>
2023-01-03 22:32:00 +0900

    feat: 회원가입 이메일 비밀번호 유효성 검사 및 버튼 컴포넌트 경로 변경

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import FormWrap from "../components/auth/FormWrap";
-import Button from "../components/common/Button";
+import Button from "../components/auth/Button";
 import AuthInput from "../components/auth/AuthInput";
 import styles from "../style/pageStyle/JoinPage.module.css";
 import classNames from "classnames/bind";
@@ -8,17 +8,52 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const JoinPage = () => {
+	const [emailValid, setEmailValid] = useState(false);
+	const [passwordValid, setPasswordValid] = useState(false);
+
+	const emailValidCheck = ({ target }) => {
+		const emailValue = target.value;
+		const emailRegex =
+			/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+		if (!emailRegex.test(emailValue)) {
+			setEmailValid(false);
+			return;
+		} else {
+			setEmailValid(true);
+		}
+	};
+	const passwordValidCheck = ({ target }) => {
+		const passwordValue = target.value;
+
+		if (passwordValue.length >= 8) {
+			setPasswordValid(true);
+		} else {
+			setPasswordValid(false);
+		}
+	};
+
 	return (
 		<main className={cx("wrap")}>
 			<h1 className={cx("title")}>회원가입</h1>
 			<FormWrap>
-				<AuthInput type="text" inputName="id" inputID="myID">
-					아이디
+				<AuthInput
+					type="text"
+					inputName="id"
+					inputID="myEmail"
+					onChange={emailValidCheck}
+				>
+					이메일
 				</AuthInput>
-				<AuthInput type="password" inputName="pw" inputID="myPW">
+				<AuthInput
+					type="password"
+					inputName="pw"
+					inputID="myPW"
+					onChange={passwordValidCheck}
+				>
 					비밀번호
 				</AuthInput>
-				<Button>가입하기</Button>
+				<Button disabled={!(emailValid && passwordValid)}>가입하기</Button>
 			</FormWrap>
 		</main>
 	);
