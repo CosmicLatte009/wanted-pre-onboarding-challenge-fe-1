8a53beec2f2c86e6fd1333d684cb247caad31b91
CosmicLatte009 <ohhm009@gmail.com>
2023-01-15 16:07:00 +0900

    feat: TodoDataContext 내에 tododata state, pathId state 생성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import TodoDataContextProvider from "./context/TodoDataContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<TodoDataContextProvider>
+			<App />
+		</TodoDataContextProvider>
 	</React.StrictMode>
 );
