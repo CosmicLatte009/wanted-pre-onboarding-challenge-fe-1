51b929dd4c26b0a29c34f796b17342f79c308841
CosmicLatte009 <ohhm009@gmail.com>
2023-01-20 14:57:00 +0900

    feat: 새 todo 버튼 기능 추가 및 todo list 받아와서 getTodoList Context 함수에 데이터 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import TodoListWrap from "../components/todo/TodoListWrap";
 import TodoDetailsWrap from "../components/todo/TodoDetailsWrap";
@@ -6,36 +6,75 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "../style/pageStyle/HomePage.module.css";
 import classNames from "classnames/bind";
+import { TodoDataContext } from "../context/TodoDataContext";
 
 const cx = classNames.bind(styles);
 
 const HomePage = () => {
+	const ctx = useContext(TodoDataContext);
+	const todoList = useMemo(() => {
+		return ctx.todoDatas;
+	}, [ctx.todoDatas]);
+
+	const [openTodoDetails, setOpenTodoDetails] = useState(false);
 	const navigate = useNavigate();
+
+	const handleGetTodoList = async () => {
+		const url = "http://localhost:8080/todos";
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Authorization: localStorage.getItem("userInfo"),
+				},
+			});
+			const result = await response.json();
+
+			if ("data" in result) {
+				ctx.getTodoList(result.data);
+				return;
+			}
+		} catch (error) {
+			console.log("error", error);
+		}
+	};
+
 	const handleLogoutBtn = () => {
 		localStorage.removeItem("userInfo");
 		navigate("/login");
 	};
+
+	const handleCreateBtnClicked = () => {
+		ctx.getPath(undefined);
+		setOpenTodoDetails(true);
+	};
+
 	useEffect(() => {
 		if (localStorage.getItem("userInfo") === null) {
 			navigate("/login");
 			alert("로그인해주세요");
+		} else {
+			handleGetTodoList();
 		}
-	}, [navigate]);
+	}, [navigate, todoList]);
+
 	return (
 		<>
 			<main className={cx("wrap")}>
 				<h1 className={cx("title")}>TODO</h1>
-				<button className={cx("btn-create")}>
+				<button className={cx("btn-create")} onClick={handleCreateBtnClicked}>
 					<FontAwesomeIcon
 						className={cx("icon-create")}
 						icon={faCirclePlus}
 						size="2x"
 					/>
-					<span>할 일 추가</span>
+					<span>새 할 일</span>
 				</button>
-				<div className={cx("todo-container")}>
-					<TodoListWrap />
-					<TodoDetailsWrap />
+				<div className={cx("todo-container", { closed: !openTodoDetails })}>
+					<TodoListWrap onTodoClicked={() => setOpenTodoDetails(true)} />
+					{openTodoDetails && (
+						<TodoDetailsWrap onClick={() => setOpenTodoDetails(false)} />
+					)}
 				</div>
 				<strong className={cx("logout")} onClick={handleLogoutBtn}>
 					로그아웃
